edae1f173b4cf23c8c97336e1d825dd860c8e667
bingoring <ritch1530@gmail.com>
2021-10-05 01:29:56 +0900

    first commit

---
+++ src/main/java/springstudygroup/springstudyfirst/SpringStudyFirstApplication.java
@@ -0,0 +1,13 @@
+package springstudygroup.springstudyfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudyFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springstudygroup/springstudyfirst/SpringStudyFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package springstudygroup.springstudyfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
